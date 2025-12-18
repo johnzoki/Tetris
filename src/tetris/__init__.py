8fa128c7bbdb6b2d8fd9ba85bcb738a8774b1eb1
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2025-12-18 21:50:14 +0100

    feat: basic game state

--- src/tetris/__init__.py
+++ src/tetris/__init__.py
@@ -1,2 +1,19 @@
+from enum import Enum
+
+Board = list[list[int]]
+PieceMap = dict[int, PieceType]
+NextQueue = list[PieceType]
+
+
+class PieceType(Enum):
+    Cube = 1
+    T = 2
+    I = 3
+    S = 4
+    Sm = 5
+    L = 6
+    Lm = 7
+
+
 def main() -> None:
     print("Hello from tetris!")
---
+++ src/tetris/game_state.py
@@ -0,0 +1,9 @@
+from tetris import Board, NextQueue, PieceMap, PieceType
+
+
+class Gamestate:
+    def __init__(self, *args):
+        self.board: Board = []
+        self.piece_queue: NextQueue = []
+        self.piece_map: PieceMap = []
+        self.hold_piece: PieceType | None = None
